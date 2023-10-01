5733b3363dd84627ae0306870f63dce0f42315ca
Amerey <info@amerey.eu>
2023-10-01 13:10:29 +0200

    nexT13

--- next.config.js
+++ next.config.js
@@ -3,10 +3,3 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
-// const path = require("path");
-
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-// };
