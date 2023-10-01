e6ffaf9b7ab57b0ac1884910913084f8ac65a0cb
Amerey <info@amerey.eu>
2023-10-01 15:19:59 +0200

    nexT13

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "../../public/arrow.svg";
+import Logo from "@/public/arrow.svg";
 
 function Navbar() {
   return (
